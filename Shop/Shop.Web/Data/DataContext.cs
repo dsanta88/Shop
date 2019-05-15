ef8815d14b957925cb86aad7dd7b2eb042c488f3
David Fernando Santa <santadavid88@hotmail.com>
2019-05-15 18:32:20 -0500

    Shop aplicacion

---
+++ Shop/Shop.Web/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Shop.Web.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+    using Shop.Web.Data.Entities;
+
+    public class DataContext: IdentityDbContext<User>
+    {
+        public DbSet<Product> Products { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
